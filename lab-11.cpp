6390b7b9e315076b923792f122b9b5d7ec82f976
Yuyi Chen <c1771463412@gmail.com>
2026-09-20 20:38:28 -0700

    Code inputStudent()

--- lab-11.cpp
+++ lab-11.cpp
@@ -4,7 +4,43 @@
 #include <string>
 using namespace std;
 
+struct Student {
+    string name;
+    int id;
+    int numGrades;
+    int *grades;
+};
+
+void inputStudent(Student *);
+void displayStudent(Student *);
+void deleteStudentData(Student *);
+
 int main() {
+    int numStudents;
+
+    cout << "How many students? ";
+    cin >> numStudents;
+    cin.ignore();
+
+    Student *students = new Student[numStudents];
 
     return 0;
+}
+
+// inputStudent() gets information and grades for one student
+// arguments: pointer to a Student
+// returns: nothing
+void inputStudent(Student *sptr) {
+    cout << "Name: ";
+    getline(cin, sptr->name);
+
+    cout << "ID: ";
+    cin >> sptr->id;
+
+    cout << "How many grades? ";
+    cin >> sptr->numGrades;
+
+    sptr->grades = new int[sptr->numGrades];
+
+
 }
